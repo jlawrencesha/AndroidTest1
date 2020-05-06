1a0a2cd8807355a966a31fb4a7d6b5f3191ad158
jlawrencesha <44271286+jlawrencesha@users.noreply.github.com>
2020-05-06 11:36:41 -0700

    added a job

--- app/src/main/java/com/giftofhappiness/androidtest/MainActivity.kt
+++ app/src/main/java/com/giftofhappiness/androidtest/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var name =  "jonnyB"
+        var fun = "I'm having fun."
     }
 }
